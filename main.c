bf46c92d81cd61576e2a6039b56f47aed510b818
dannyf00 <fdan00@live.com>
2020-07-04 15:26:29 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -1,49 +1,42 @@
-#include "gpio.h"									//use gpio
-#include "delay.h"									//use software delays
-#include "coretick.h"
-#include "benchmark_ti.h"
-#include "dhry.h"									//we use dhrystone benchmark
-#include "whetstone.h"								//we use whetstone benchmark
+//#include "gpio.h"					//we use F_CPU + gpio functions
+//#include "delay.h"					//we use software delays
+//#include "systick.h"				//we use systick
+#include "dhry.h"					//we use dhrystone benchmark
+#include "whetstone.h"				//we use whetstone benchmark
+#include "benchmark_ti.h"			//we use TI's benchmark routines
 
 //hardware configuration
-#define LED_PORT			GPIOF
-#define LED					(LEDG)					//1=R, 2=B, 3=G
 //end hardware configuration
 
 //global defines
 
 //global variables
 
+
 int main(void) {
-	int32_t i, time1, tmp;
+	uint32_t time0, time1, tmp;
+	int32_t index;
 
-	mcu_init();										//reset the  mcu
-	coretick_init();									//initialize systick
+	//mcu_init();									//reset the  mcu
 
-	IO_OUT(LED_PORT, LED);							//led as output
-
-	//ei();											//enable interrupts
-	while(1) {
-		//flip led
-		IO_FLP(LED_PORT, LED);						//to be benchmarked
+	//IO_OUT(LED_PORT, LED);						//led3/4 as output
+	//ei();
+	while (1) {
+		//IO_FLP(LED_PORT, LED);					//flip led
 
 		//benchmark starts
-		time1=benchmark(main_8);
-		time1=benchmark(main_8m);
-		time1=benchmark(main_8sw);
-		time1=benchmark(main_16);
-		time1=benchmark(main_16m);
-		time1=benchmark(main_16sw);
-		time1=benchmark(main_32);
-		time1=benchmark(main_f);
-		time1=benchmark(main_mmlt);
-		time1=benchmark(main_fir);
+		time1=benchmark(main_8);							//8-bit Math
+		time1=benchmark(main_8m);							//8-bit Matrix
+		time1=benchmark(main_8sw);							//8-bit Switch
+		time1=benchmark(main_16);							//16-bit Math
+		time1=benchmark(main_16m);							//16-bit Matrix
+		time1=benchmark(main_16sw);							//16-bit Switch
+		time1=benchmark(main_32);							//32-bit Math
+		time1=benchmark(main_f);							//floating point Math
+		time1=benchmark(main_mmlt);							//floating point Matrix
+		time1=benchmark(main_fir);							//floating point FIR
 		time1=benchmark(dhrystone);							//dyrstone benchmark
 		time1=benchmark(whetstone);							//whetseone benchmark
-
 	}
-
-	return 0;
 }
-
 
